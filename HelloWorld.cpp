25f44efcbd8616ef5a8b2bc4940b5006a9bace5c
imadhb <imudps@gmail.com>
2024-09-13 18:25:07 -0700

    Adding Author Name

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,5 +1,6 @@
 // Simple C++ program to display "Hello World" 
 // Header file for input output functions 
+//Author: Imad Hoballah <hh2247@wayne.edu>
 #include<iostream>  
   
 using namespace std; 
